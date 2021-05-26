d48bcfdbf6d77e28afcf614bdd54a582b6266f3d
arowent <arowent@mail.ru>
2021-05-26 14:17:35 +0300

    Config delete

--- config.py
+++
@@ -1,3 +0,0 @@
-bot_token = '1807126251:AAHHiWqfWLSv5iyKbB9TY1kVyZfayFpZ35o'
-genius_token = 'h0k3DuukYfq7LzuevVPmD6n7MlzE-CI-fXYW3F1512svr2vEOZhvdSAlHq4T2Uag'
-donate = 'https://yoomoney.ru/to/4100116813383560'
